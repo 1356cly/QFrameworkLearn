27d217e7927fb3ce95288a93279aec4a781b4d40
1356cly <axyko1356@163.com>
2026-08-10 17:51:26 +0800

    hello world Test

---
+++ QFrameworkLearn/Assets/Scripts/helloworld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!This is my first time to learn QFrameWork");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
